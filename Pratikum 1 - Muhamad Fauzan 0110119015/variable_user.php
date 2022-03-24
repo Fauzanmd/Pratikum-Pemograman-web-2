2862a3cfe7d333e74caed15ca6c17fbb6bc3becc
Fauzan Muhamad <72536321+Fauzanmd@users.noreply.github.com>
2022-03-25 01:00:10 +0700

    Add files via upload

---
+++ Pratikum 1 - Muhamad Fauzan 0110119015/variable_user.php
@@ -0,0 +1,13 @@
+<?php
+// definisikan variables
+$nama = 'Fauzan';
+$umur = 21;
+$berat = 51;
+
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+
+echo "<br/>Hello $nama Apakabar";
+
+?>
