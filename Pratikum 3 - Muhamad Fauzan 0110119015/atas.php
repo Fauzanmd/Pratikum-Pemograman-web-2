db2ea592aa9a39507bd0309a29052cf2aad89be3
Fauzan Muhamad <72536321+Fauzanmd@users.noreply.github.com>
2022-03-30 14:07:22 +0700

    Add files via upload

---
+++ Pratikum 3 - Muhamad Fauzan 0110119015/atas.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <title>Student Activity Score</title>
+</head>
+<body>
+    <header role="banner">
+    <h2>Dashboard</h2>
+ </header>
